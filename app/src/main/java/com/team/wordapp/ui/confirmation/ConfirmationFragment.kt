5fa8921e2258692d00778ce2bf6d0eccd88ae57a
JeromeE <jeremyho885@gmail.com>
2025-09-05 15:51:45 +0800

    Finished sort, confirmation changed to dialog, searchbar, item layout and others minor tweaks

--- app/src/main/java/com/team/wordapp/ui/confirmation/ConfirmationFragment.kt
+++ app/src/main/java/com/team/wordapp/ui/confirmation/ConfirmationFragment.kt
@@ -2,17 +2,17 @@ package com.team.wordapp.ui.confirmation
 
 import androidx.fragment.app.viewModels
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.setFragmentResult
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.team.wordapp.R
 import com.team.wordapp.databinding.FragmentConfirmationBinding
 
-class ConfirmationFragment: Fragment() {
+class ConfirmationFragment: DialogFragment() {
     private lateinit var binding: FragmentConfirmationBinding
     private val viewModel: ConfirmationViewModel by viewModels()
     private val args: ConfirmationFragmentArgs by navArgs()
--- app/src/main/java/com/team/wordapp/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/team/wordapp/ui/home/HomeViewModel.kt
@@ -10,15 +10,34 @@ class HomeViewModel(
     private val repo: WordRepo = WordRepo.getInstance()
 ): ViewModel() {
 
+    var searchState = ""
+    var sortState1 = 0
+    var sortState2 = 0
     private val _words = MutableStateFlow<List<Word>>(emptyList())
     val words = _words.asStateFlow()
 
+    fun List<Word>.searchSort(search: String, sort1: Int, sort2: Int): List<Word> {
+        var list = this
+        if (search.isNotEmpty()) { list = list.filter { it.title.contains(search, ignoreCase = true) } }
+        list = when (sort2) {
+            0 -> {
+                if (sort1 == 0) list.sortedBy { it.title }
+                else list.sortedByDescending { it.title }
+            }
+            1 -> {
+                if (sort1 == 0) list.sortedBy { it.createdAt }
+                else list.sortedByDescending { it.createdAt }
+            }
+            else -> list
+        }
+        return list
+    }
+
     init {
-        getWords()
+        getWords(searchState, sortState1, sortState2)
     }
 
-    fun getWords() {
-        _words.value = repo.getAllWords()
+    fun getWords(search: String = searchState, sort1: Int = sortState1, sort2: Int = sortState2) {
+        _words.value = repo.getAllWords().searchSort(search, sort1, sort2)
     }
-
 }
