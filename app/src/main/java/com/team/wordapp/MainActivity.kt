165099a2503005547cd45188f221413efb256154
Keshen Nareshchandra <njkeshen98@gmail.com>
2025-09-09 10:31:15 +0800

    Created animations for all pages and ensured only light theme is applied for all users.

--- app/src/main/java/com/team/wordapp/MainActivity.kt
+++ app/src/main/java/com/team/wordapp/MainActivity.kt
@@ -3,11 +3,14 @@ package com.team.wordapp
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        // Forces light mode, even if the phone has dark mode settings turned on
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
